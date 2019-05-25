255c09aed90420fd8261e1695b8f97e69ec4c58c
Fabrice Daugan <fabrice.daugan@gmail.com>
2019-05-25 16:44:38 +0200

    Fix cache retention duration for 'user-is-admin'

--- src/main/java/org/ligoj/app/plugin/id/resource/IdCache.java
+++ src/main/java/org/ligoj/app/plugin/id/resource/IdCache.java
@@ -5,6 +5,9 @@ package org.ligoj.app.plugin.id.resource;
 
 import java.util.function.Function;
 
+import javax.cache.expiry.Duration;
+import javax.cache.expiry.ModifiedExpiryPolicy;
+
 import org.ligoj.bootstrap.resource.system.cache.CacheManagerAware;
 import org.springframework.stereotype.Component;
 
@@ -20,7 +23,9 @@ public class IdCache implements CacheManagerAware {
 	@Override
 	public void onCreate(final HazelcastCacheManager cacheManager, final Function<String, CacheConfig<?, ?>> provider) {
 		cacheManager.createCache("container-scopes", provider.apply("container-scopes"));
-		cacheManager.createCache("user-is-admin", provider.apply("user-is-admin"));
+		final var isAdmin = provider.apply("user-is-admin");
+		isAdmin.setExpiryPolicyFactory(ModifiedExpiryPolicy.factoryOf(Duration.ONE_MINUTE));
+		cacheManager.createCache("user-is-admin", isAdmin);
 		cacheManager.createCache("id-configuration", provider.apply("id-configuration"));
 	}
 
