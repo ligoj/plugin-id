3762173c9db6927dcfb4deda65179a739e9b71fe
Fabrice Daugan <fabrice.daugan@gmail.com>
2026-08-29 14:24:29 +0200

    chore(deps): Bump plugin-api to 5.0.0

--- src/main/java/org/ligoj/app/plugin/id/resource/IdCache.java
+++ src/main/java/org/ligoj/app/plugin/id/resource/IdCache.java
@@ -6,6 +6,8 @@ package org.ligoj.app.plugin.id.resource;
 import com.hazelcast.cache.HazelcastCacheManager;
 import org.ligoj.bootstrap.resource.system.cache.CacheConfigurer;
 import org.ligoj.bootstrap.resource.system.cache.CacheManagerAware;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Role;
 import org.springframework.stereotype.Component;
 
 import javax.cache.expiry.Duration;
@@ -14,6 +16,7 @@ import javax.cache.expiry.Duration;
  * Cache configuration for IAM.
  */
 @Component
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class IdCache implements CacheManagerAware {
 
 	@Override
